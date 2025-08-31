1485f63416d3d4281c6bb447da4564545b4b1487
charlesLoder <charles.w.loder@gmail.com>
2025-08-30 21:36:18 -0400

    Add tools to dev page

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+import { Calculator } from "@langchain/community/tools/calculator";
 import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_run";
 import Viewer from "@samvera/clover-iiif/viewer";
 import { StrictMode } from "react";
@@ -10,6 +11,8 @@ const wiki_tool = new WikipediaQueryRun({
   topKResults: 3,
   maxDocContentLength: 4000,
 });
+
+const calc_tool = new Calculator();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -32,7 +35,7 @@ createRoot(document.getElementById("root")!).render(
               en: ["AI Chat"],
             },
             componentProps: {
-              provider: new UserTokenProvider({ tools: [wiki_tool] }),
+              provider: new UserTokenProvider({ tools: [wiki_tool, calc_tool] }),
             },
           },
         },
