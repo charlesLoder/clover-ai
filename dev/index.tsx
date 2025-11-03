b309f5324f24d365300d0a4ccfe87373f37aeae9
charlesLoder <charles.w.loder@gmail.com>
2025-11-03 14:58:19 -0500

    Vendor langchain tools

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,18 +1,15 @@
 /// <reference types="vite/client" />
-import { Calculator } from "@langchain/community/tools/calculator";
-import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_run";
 import Viewer from "@samvera/clover-iiif/viewer";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { PluginControl, PluginPanel } from "../src/plugin";
 import { UserTokenProvider } from "../src/providers";
+import { WikipediaQueryRun } from "./tools/wikipedia-query-run";
 
 const wiki_tool = new WikipediaQueryRun({
   topKResults: 3,
   maxDocContentLength: 4000,
 });
-
-const calc_tool = new Calculator();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -35,7 +32,7 @@ createRoot(document.getElementById("root")!).render(
               en: ["AI Chat"],
             },
             componentProps: {
-              provider: new UserTokenProvider({ tools: [wiki_tool, calc_tool] }),
+              provider: new UserTokenProvider({ tools: [wiki_tool] }),
             },
           },
         },
