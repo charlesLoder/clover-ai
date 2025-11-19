a7589eaa2a9b7920de56d6c0bf652608bf7af369
charlesLoder <charles.w.loder@gmail.com>
2025-11-19 10:45:13 -0500

    Add bullet list icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,7 +1,6 @@
-import { Add } from "./Add";
-import { ArrowUp } from "./ArrowUp";
-import { Clear } from "./Clear";
-import { Close } from "./Close";
-import { Gear } from "./Gear";
-
-export { Add, ArrowUp, Clear, Close, Gear };
+export { Add } from "./Add";
+export { ArrowUp } from "./ArrowUp";
+export { BulletList } from "./BulletList";
+export { Clear } from "./Clear";
+export { Close } from "./Close";
+export { Gear } from "./Gear";
